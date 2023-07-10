89ff13ad712e6076cdd55361e373aa44f2bbd35b
LenyaPlay <lenyaplay@mail.ru>
2023-07-11 02:04:45 +0300

    done

--- App.tsx
+++ App.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { WebView } from 'react-native-webview';
+import { Alert, NativeModules, Platform, Text } from 'react-native';
+import ReactNativeIdfaAaid, { AdvertisingInfoResponse } from '@sparkfabrik/react-native-idfa-aaid';
+
+import messaging from '@react-native-firebase/messaging';
+
+
 
 const MyWebView = () => {
   const url = "https://pagbeting.space/QN9Kbb";
   const url2 = "https://decodeit.ru/qr";
-  const gaid = ""
-  const token = "";
+
+  const [gaid, setGaid] = useState<string | null>();
+  const [token, setToken] = useState<string>();
+  const name = "Danil_Khaertdinov";
+  const [loaded, setLoaded] = useState<Boolean>(false);
+
+  const loadData = async () => {
+    try{
+      let res = await ReactNativeIdfaAaid.getAdvertisingInfo();
+      setGaid(res.id);
+      let token =  await messaging().getToken();
+      setToken(token);
+  
+      console.log(`${res.id} \n ${token}`);
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  const getFullUrl = () => `${url}?gaid=${gaid}&token=${token}&name=${name}`;
+
+  React.useEffect(() => {
+    loadData();
+  }, []);
+
+  //Надо было на gaid && token поменять, но я уже скомпилировал и было лень(
   return (
-    <WebView source={{ uri: url2 }} style={{ flex: 1 }} />
+    <>
+      {token && <Text>{getFullUrl()}</Text>}
+      {token && <WebView source={{ uri: getFullUrl() }} style={{ flex: 1 }} />} 
+    </>
   );
+  
 };
 
 export default MyWebView;
